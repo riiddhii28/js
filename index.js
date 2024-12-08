3d2347ec14c3c5ba28b0e28460003e546784968a
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-09 00:36:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("This is my first line");
+
+window.alert("This is an alert");
+
+//one line
+
+/*
+multiple
+lines
+*/
+
+document.getElementById("myH1").textContent="Hello";
+document.getElementById("myP").textContent="This is demo";
+
