3773f3ff63f32273b1b2fe5601f2ae613693f5a1
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-10 00:41:32 +0530

    commit message

---
+++ variables.js
@@ -0,0 +1,20 @@
+let x=100;
+
+console.log(x);
+
+let age=20;
+let price=10.99;
+let gpa=9.5;
+
+let firstName="Riddhi";
+let lastName="Patel";
+
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(gpa);
+
+console.log(typeof price);
+console.log(typeof firstName);
+
+console.log(firstName);
+console.log(`My last name is ${lastName}`);
