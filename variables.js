8545b781ecd279b2ea9bfb7c300b13652dd76201
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-10 22:41:02 +0530

    commit message

--- variables.js
+++ variables.js
@@ -8,6 +8,10 @@ let gpa=9.5;
 
 let firstName="Riddhi";
 let lastName="Patel";
+let email="riddhi123@gmail.com";
+
+let online=true;
+let forSale=false;
 
 console.log(`You are ${age} years old`);
 console.log(`The price is ${price}`);
@@ -18,3 +22,13 @@ console.log(typeof firstName);
 
 console.log(firstName);
 console.log(`My last name is ${lastName}`);
+console.log(email);
+
+console.log(typeof online);
+console.log(`Is this car for sale?: ${forSale}`);
+
+document.getElementById("p1").textContent= firstName;
+document.getElementById("p2").textContent= lastName;
+document.getElementById("p3").textContent= online;
+
+document.getElementById("p4").textContent= `Your name is ${firstName}`;
