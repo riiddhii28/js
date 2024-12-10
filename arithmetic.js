acac0fc48038e9333da3283d1614b3e7ce86222e
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-10 22:57:06 +0530

    commit message

---
+++ arithmetic.js
@@ -0,0 +1,50 @@
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students % 2;
+
+//students = students ** 2;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+
+//students **= 2;
+
+//students %= 2;
+
+//students++:
+//students--:
+
+
+//console.log(students);
+
+/*
+operator precedence:
+1. parentheses ()
+2. exponents
+3. multi and div and modulo
+4. add sub
+*/
+
+let result= 1 + 2 * 3 + 4 ** 2;
+console.log(result);
+
+//16+6+1
+
+let result2= 12 % 5 + 8 / 2;
+console.log(result2);
+
+//2+4
+
+let result3= 6 / 2 ** 7;
+console.log(result3);
+
+//6/128
+
+
+
