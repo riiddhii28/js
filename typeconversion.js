4535bba059698fa388e72fa139bdb65d8e1e5f2d
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-15 23:20:33 +0530

    commit message

---
+++ typeconversion.js
@@ -0,0 +1,22 @@
+//strings, numbers, booleans
+
+/*
+let age=window.prompt("How old?");
+age = Number(age);
+age+=1;
+
+console.log(age, typeof age);
+*/
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
