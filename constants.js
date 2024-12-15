c527cf7056cf048594646965a2ae328d3b426f19
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-15 23:25:16 +0530

    commit message

---
+++ constants.js
@@ -0,0 +1,10 @@
+const pi = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter radius");
+
+radius = Number(radius);
+
+circumference = 2*pi*radius;
+console.log(circumference);10
