898659ec63089784497d8a20d429a7821e03bb61
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-16 23:15:58 +0530

    commit message

---
+++ ternary.js
@@ -0,0 +1,18 @@
+/*
+let age = 21;
+let message = age >= 18 ? "Youre an adult" : "youre a minor";
+console.log(message);
+*/
+
+/*
+let time = 16;
+let greeting = time < 12 ? "Good morning" : "Good afternoon";
+console.log(greeting); // Good afternoon
+*/
+
+
+let isStudent = false;
+let message = isStudent ? "Youre student" : "not student"
+console.log(message);
+
+
