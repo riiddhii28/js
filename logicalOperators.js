8a7ff87eeb0a65014b1cd9f44f00c992d03e286b
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-17 23:25:51 +0530

    commit message

---
+++ logicalOperators.js
@@ -0,0 +1,35 @@
+//used to manipulate boolean (true false)
+
+// AND = &&
+// OR = ||
+// NOT = !
+
+/*
+const temp = 200;
+
+if(temp > 0 && temp <= 30){
+    console.log("Weather is good");
+}
+else{
+    console.log("Weather is bad");
+}
+*/
+
+/*
+const temp = 200;
+
+if(temp < 0 || temp >= 30){
+    console.log("Weather is BAD");
+}
+else{
+    console.log("Weather is GOOD");
+}
+*/
+
+const isSunny = false;
+
+if(!isSunny){
+    console.log("its cloudy");
+}else{
+    console.log("its sunny");
+}
