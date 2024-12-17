833d5fd8c855d832aeeb7e62c1841fbcfe6db1c9
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-12-17 23:33:27 +0530

    commit message

---
+++ strictEquality.js
@@ -0,0 +1,23 @@
+// = assignment operator
+// == comparison operator (if values are equal)
+// === strict equality (id values & datatype are equal)
+// != inequality operator
+// !== strict inequality opertaor
+
+/*
+const pi = 3.14;
+if(pi === "3.14"){
+    console.log("that is PI");
+}
+else{
+    console.log("that is not PI"); //this is output
+}
+*/
+
+const pi = 3.14;
+if(pi !== "3.14"){
+    console.log("that is not PI");
+}
+else{
+    console.log("that is PI"); 
+}
