df232ccd19ad5c03811133f341b8cb9aa8356b8b
Riddhi Patel <riddhipatel.riki@gmail.com>
2024-12-19 00:01:20 +0530

    commit message

---
+++ scope.js
@@ -0,0 +1,31 @@
+//variable scope = local OR global (where a variable is recognised and accessed)
+
+/*
+//local
+function1();
+function2();
+
+function function1(){
+    let x = 1;
+    console.log(x);
+}
+
+function function2(){
+    let x = 2;
+    console.log(x);
+}
+*/
+
+//global
+let x = 3;
+
+function1();
+function2();
+
+function function1(){
+    console.log(x);
+}
+
+function function2(){
+    console.log(x);
+}
