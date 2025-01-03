a9e0a52309915270760e5c0f178b591d5fe9e7f1
Riddhi Patel <riddhipatel.riki@gmail.com>
2025-01-03 15:58:27 +0530

    commit message

--- callback.js
+++ callback.js
@@ -2,5 +2,39 @@
 
 //used to handle asynchronous operations.
 
-hello();
+hello(wait);
 
+
+//resukt: 
+//Hello
+//Wait
+
+function hello(callback){
+    console.log("Hello");
+    callback();
+}
+
+function leave(){
+    console.log("leave");
+}
+
+function wait(){
+    console.log("wait");
+}
+
+
+function goodbye(){
+    console.log("goodbye");
+}
+
+//new
+sum(display, 5, 2);
+
+function sum(callback, x, y){
+    let result = x + y;
+    callback(result);
+}
+
+function display(result){
+    console.log(result);
+}
